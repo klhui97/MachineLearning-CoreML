308b98fc3dafc7dd9b71db26b72da8647baed2b7
KL <mrdavidhui1997@gmail.com>
2018-06-13 23:22:34 +0800

    init LinearRegression

---
+++ MachineLearningProject/LinearRegression/LinearRegressionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LinearRegressionViewController.swift
+//  MachineLearningProject
+//
+//  Created by KL on 13/6/2018.
+//  Copyright © 2018 KL. All rights reserved.
+//
+
+import UIKit
+
+class LinearRegressionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MachineLearningProject/LinearRegression/README.md
@@ -0,0 +1,10 @@
+## Source
+```
+https://code.tutsplus.com/tutorials/how-to-train-a-core-ml-model-for-an-ios-app--cms-30147
+```
+
+
+## Python Libraries
+```
+sudo -H pip install --ignore-installed coremltools scikit-learn pandas
+```
